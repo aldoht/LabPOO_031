cd046b24d088285c4f131a03965db3e9dc610bd9
aldof <aldof@home-001>
2023-08-13 21:26:47 -0600

    Primera práctica

---
+++ Practica1/src/main/java/com/mycompany/practica1/Practica1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.practica1;
+
+/**
+ *
+ * @author aldof
+ */
+public class Practica1 {
+
+    public static void main(String[] args) {
+        System.out.println("¡Hola, mundo!");
+    }
+}
