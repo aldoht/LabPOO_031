4c4f0639cf1df6c2d381e94e1a6ad78e56862619
aldof <aldof@home-001>
2023-09-10 20:59:33 -0600

    Práctica 5 del Laboratorio de POO

--- Practica5/src/main/java/org/example/Main.java
+++ Practica5/src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import javax.print.Doc;
 public class Main {
     public static void main(String[] args) {
         float promedio = 0;
-        System.out.println("Hello world!");
+        System.out.println("Hello, world!");
 
         Persona dr = new Doctor("Rebeca");
         Persona st = new Estudiante("Aldo");
