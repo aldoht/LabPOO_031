59562f02b5ad44ecafb528ca96f8c5a66791fdd2
aldof <aldof@home-001>
2023-10-12 20:31:51 -0600

    Práctica 7 del Laboratorio de Programación Orientada a Objetos.

--- Practica7/src/main/java/Arreglos/PracticaArreglos.java
+++ Practica7/src/main/java/Arreglos/PracticaArreglos.java
@@ -12,16 +12,16 @@ public class PracticaArreglos {
         name[3] = "Luis";
         name[4] = "Brad";
         for (int i = 0; i < name.length; i++) {
-            System.out.println("Nombre " + Integer.toString(i+1) + ": " + name[i]);
+            System.out.println("Nombre " + (i + 1) + ": " + name[i]);
         }
     }
     public void leerAutoArray() {
         Automoviles[] auArr = new Automoviles[5];
         for (int i = 0; i < auArr.length; i++) {
             auArr[i] = new Automoviles();
-            System.out.println("Ingrese la marca del auto " + Integer.toString(i+1) + ':');
+            System.out.println("Ingrese la marca del auto " + (i+1) + ':');
             auArr[i].setMarca(sc.nextLine());
-            System.out.println("Ingrese el modelo del auto " + Integer.toString(i+1) + ':');
+            System.out.println("Ingrese el modelo del auto " + (i+1) + ':');
             auArr[i].setModelo(sc.nextLine());
         }
         for (Automoviles au: auArr) {
